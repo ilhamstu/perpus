19cecc6ffcdb7c9f12ec99d704a775100064b6ec
ilhamstu <muhammad.ilham@students.amikom.ac.id>
2022-02-02 00:54:26 +0700

    update Modal CRUD data item

--- application/views/admin/item.php
+++ application/views/admin/item.php
@@ -14,7 +14,7 @@
             <th>Kategori</th>
             <th>Stok</th>
             <th>Rak</th>
-            <th colspan="2">Aksi</th>
+            <th>Aksi</th>
         </thead>
         <tbody>
             <?php
@@ -32,10 +32,17 @@
                 <td><?php echo $c_buku->nmKategori ?></td>
                 <td><?php echo $c_buku->stok ?></td>
                 <td><?php echo $c_buku->rak ?></td>
-                <td><?php echo anchor('admin/edit_buku/'.$c_buku->kdBuku,'<div class="btn btn-sm btn-primary"><i class="fas fa-edit"></i></div>') ?>
-                </td>
-                <td onclick="javascript: return confirm('Aksi ini tidak bisa dikembalikan, yakin ingin menghapus?')">
-                    <?php echo anchor('admin/hapus_buku/'.$c_buku->kdBuku,'<div class="btn btn-sm btn-danger"><i class="fas fa-trash"></i></div>') ?>
+                <td>
+                    <!-- <?php echo anchor('admin/edit_buku/'.$c_buku->kdBuku,'<div class="btn btn-sm btn-primary"><i class="fas fa-edit"></i></div>') ?> -->
+                    <!-- <?php echo anchor('admin/hapus_buku/'.$c_buku->kdBuku,'<div class="btn btn-sm btn-danger"><i class="fas fa-trash"></i></div>') ?> -->
+                    <button type="button" class="btn btn-sm btn-primary" id="edit" data-jdl="<?=$c_buku->jdlBuku?>"
+                        data-id="<?=$c_buku->kdBuku?>" data-kdktgr="<?php echo $c_buku->kdKategori ?>"
+                        data-ktgr="<?=$c_buku->nmKategori?>" data-stok="<?=$c_buku->stok?>" data-rak="<?=$c_buku->rak?>"
+                        data-toggle="modal" data-target="#exampleModal"><i class="fas fa-edit"></i>
+                    </button>
+                    <a href="<?=base_url('admin/hapus_buku/'.$c_buku->kdBuku)?>"
+                        onclick="javascript: return confirm('Aksi ini tidak bisa dikembalikan, yakin ingin menghapus?')"
+                        class="btn btn-sm btn-danger"><i class="fas fa-trash"></i></a>
                 </td>
             </tr>
             <?php } ?>
@@ -46,9 +53,58 @@
 
 </section>
 
+<!-- Modal Edit-->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Form Edit Buku</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form action="<?php echo base_url().'admin/update_buku'; ?>" method="post">
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label>Judul Buku</label>
+                        <input type="hidden" id="id" name="id" class="form-control">
+                        <input type="text" id="jdl" name="jdl" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Stok</label>
+                        <input type="text" id="stok" name="stok" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Rak</label>
+                        <input type="text" id="rak" name="rak" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label>Kategori</label>
+                        <select class="custom-select" name="kategori">
+                            <option id="skat"></option>
+                            <?php 
+                              foreach ($ktgr as $c_ktgr) {
+                                  ?>
+                            <option value="<?php echo $c_ktgr->kdKategori ?>">
+                                <?= $c_ktgr->nmKategori ?>
+                            </option>
+                            <?php } ?>
+                        </select>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                    <button type="submit" class="btn btn-primary">Simpan</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
 <!-- tambahbukuform -->
 <div class="modal fade" id="t_buku" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Tambah Data Buku</h5>
@@ -56,19 +112,30 @@
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <div class="modal-body">
-                <form class="form-signin" action="<?php echo base_URL()?>admin/tambah" method="post">
-                    <legend>Tambah Data</legend>
-                    <i class="nav-icon fas fa-book" style="margin-right: 10px;"></i>
-                    <input type="text" class="input-block-level" placeholder="Judul Buku" name="jdl" autofocus
-                        required><br><br>
-                    <i class="nav-icon fas fa-layer-group" style="margin-right: 10px;"></i>
-                    <input type="text" class="input-block-level" placeholder="Stok" name="stok" required><br><br>
-                    <i class="nav-icon fas fa-warehouse" style="margin-right: 10px;"></i>
-                    <input type="text" class="input-block-level" placeholder="Rak" name="rak" required><br><br>
+            <form class="form-signin" action="<?php echo base_URL()?>admin/tambah" method="post">
+                <div class="modal-body">
                     <div class="input-group mb-3">
                         <div class="input-group-prepend">
-                            <label class="input-group-text" for="inputGroupSelect01">Kategori</label>
+                            <label class="input-group-text"><i class="nav-icon fas fa-book"></i></label>
+                        </div>
+                        <input type="text" placeholder="Judul Buku" name="jdl" class="form-control" autofocus required>
+                    </div>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <label class="input-group-text"><i class="nav-icon fas fa-layer-group"></i></label>
+                        </div>
+                        <input type="text" class="form-control" placeholder="Stok" name="stok" required>
+                    </div>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <label class="input-group-text"><i class="nav-icon fas fa-warehouse"></i></label>
+                        </div>
+                        <input type="text" placeholder="Rak" name="rak" class="form-control" required>
+                    </div>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <label class="input-group-text" for="inputGroupSelect01"><i
+                                    class="nav-icon fas fa-tag"></i></label>
                         </div>
                         <select class="custom-select" id="inputGroupSelect01" name="kategori">
                             <option selected>Pilih Kategori</option>
@@ -80,13 +147,12 @@
                                 <?php echo $no++.". ".$c_ktgr->nmKategori ?></option>
                             <?php } ?>
                         </select>
-                    </div><br><br>
-                    <br><br><br>
-                    <div class="modal-footer">
-                        <button class="btn btn-large btn-primary" type="submit">Tambah</button>
                     </div>
-                </form>
-            </div>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-large btn-primary" type="submit">Tambah</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
@@ -94,7 +160,7 @@
 </div>
 <!-- tambahkategori -->
 <div class="modal fade" id="t_ktgr" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Tambah Kategori</h5>
@@ -102,18 +168,20 @@
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
-            <div class="modal-body">
-                <form class="form-signin" action="<?php echo base_URL()?>admin/tambah_ktgr" method="post">
-                    <legend>Tambah Data</legend>
-                    <i class="nav-icon fas fa-tag" style="margin-right: 10px;"></i>
-                    <input type="text" class="input-block-level" placeholder="Nama Kategori" name="ktgr" autofocus
-                        required><br><br>
-                    <br><br><br>
-                    <div class="modal-footer">
-                        <button class="btn btn-large btn-primary" type="submit">Tambah</button>
+            <form class="form-signin" action="<?php echo base_URL()?>admin/tambah_ktgr" method="post">
+                <div class="modal-body">
+                    <div class="input-group input-group-lg mb-3">
+                        <div class="input-group-prepend">
+                            <label class="input-group-text"><i class="nav-icon fas fa-tag"></i></label>
+                        </div>
+                        <input type="text" placeholder="Nama Kategori" name="ktgr" class="form-control" autofocus
+                            required>
                     </div>
-                </form>
-            </div>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-large btn-primary" type="submit">Tambah</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
--- application/views/admin/templates/footer.php
+++ application/views/admin/templates/footer.php
@@ -1,13 +1,13 @@
 <footer class="main" style="position: sticky; bottom: 0px;">
     <strong>Copyright &copy; 2014-2020 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
     All rights reserved.
-  </footer>
+</footer>
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
     <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
+</aside>
+<!-- /.control-sidebar -->
 </div>
 <!-- ./wrapper -->
 
@@ -17,7 +17,7 @@
 <script src="<?php echo base_url()?>/assets/plugins/jquery-ui/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
-  $.widget.bridge('uibutton', $.ui.button)
+$.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
 <script src="<?php echo base_url()?>/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -34,7 +34,8 @@
 <script src="<?php echo base_url()?>/assets/plugins/moment/moment.min.js"></script>
 <script src="<?php echo base_url()?>/assets/plugins/daterangepicker/daterangepicker.js"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="<?php echo base_url()?>/assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="<?php echo base_url()?>/assets/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js">
+</script>
 <!-- Summernote -->
 <script src="<?php echo base_url()?>/assets/plugins/summernote/summernote-bs4.min.js"></script>
 <!-- overlayScrollbars -->
@@ -45,3 +46,21 @@
 <script src="<?php echo base_url()?>/assets/dist/js/demo.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="<?php echo base_url()?>/assets/dist/js/pages/dashboard.js"></script>
+<script>
+$(document).ready(function() {
+    $(document).on('click', '#edit', function() {
+        var id_b = $(this).data('id');
+        var jdl = $(this).data('jdl');
+        var ktgr = $(this).data('ktgr');
+        var kdktgr = $(this).data('kdktgr');
+        var stok = $(this).data('stok');
+        var rak = $(this).data('rak');
+        $('#id').val(id_b);
+        $('#jdl').val(jdl);
+        $('#stok').val(stok);
+        $('#rak').val(rak);
+        $('#skat').val(kdktgr);
+        $('#skat').text(ktgr);
+    })
+})
+</script>
